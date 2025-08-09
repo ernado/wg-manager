eeefaf708557d5a73edb554ad172ce57f933175f
Aleksandr Razumov <ernado@go-faster.org>
2025-08-09 18:26:56 +0300

    feat: configure routing

--- cmd/wg-manager/root.go
+++ cmd/wg-manager/root.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log/slog"
+	"net"
 	"os"
+	"os/exec"
+	"strings"
+	"time"
 
 	"github.com/go-faster/errors"
 	"github.com/spf13/cobra"
@@ -11,6 +16,58 @@ import (
 	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
 	"gopkg.in/yaml.v3"
 )
+
+type Application struct {
+	log *slog.Logger
+}
+
+func (a *Application) command(ctx context.Context, name string, args ...string) error {
+	cmd := exec.CommandContext(ctx, name, args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	a.log.Info("[#] " + name + " " + strings.Join(args, " "))
+	return cmd.Run()
+}
+
+func (a *Application) iptables(ctx context.Context, args ...string) error {
+	return a.command(ctx, "iptables", args...)
+}
+
+func (a *Application) sysctl(ctx context.Context, args ...string) error {
+	return a.command(ctx, "sysctl", args...)
+}
+
+func (a *Application) deconfigureRouting(ctx context.Context, wgInterface, natInterface string) error {
+	/* iptables -D FORWARD -i wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE */
+	if err := a.iptables(ctx, "-D", "FORWARD", "-i", wgInterface, "-j", "ACCEPT"); err != nil {
+		return errors.Wrap(err, "deconfigure iptables FORWARD")
+	}
+	if err := a.iptables(ctx, "-t", "nat", "-D", "POSTROUTING", "-o", natInterface, "-j", "MASQUERADE"); err != nil {
+		return errors.Wrap(err, "deconfigure iptables POSTROUTING")
+	}
+
+	return nil
+}
+
+func (a *Application) configureRouting(ctx context.Context, wgInterface, natInterface string) error {
+	/*
+		iptables -A FORWARD -i wg0 -j ACCEPT
+		iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+		sysctl -q -w net.ipv4.ip_forward=1
+	*/
+
+	if err := a.iptables(ctx, "-A", "FORWARD", "-i", wgInterface, "-j", "ACCEPT"); err != nil {
+		return errors.Wrap(err, "configure iptables FORWARD")
+	}
+	if err := a.iptables(ctx, "-t", "nat", "-A", "POSTROUTING", "-o", natInterface, "-j", "MASQUERADE"); err != nil {
+		return errors.Wrap(err, "configure iptables POSTROUTING")
+	}
+	if err := a.sysctl(ctx, "-q", "-w", "net.ipv4.ip_forward=1"); err != nil {
+		return errors.Wrap(err, "configure sysctl net.ipv4.ip_forward")
+	}
+
+	return nil
+}
 
 func Root() *cobra.Command {
 	cmd := &cobra.Command{
@@ -36,6 +93,10 @@ func Root() *cobra.Command {
 
 			lg.Info("wg-manager started")
 
+			app := &Application{
+				log: lg,
+			}
+
 			client, err := wgctrl.New()
 			if err != nil {
 				return errors.Wrap(err, "create client")
@@ -60,6 +121,7 @@ func Root() *cobra.Command {
 			link := rtnl.NewLink()
 			link.ApplyType("wireguard")
 			link.Info.Name = "wgm0"
+			link.Info.Mtu = 1420
 
 			if err := link.Add(rx); err != nil {
 				if errors.Is(err, os.ErrExist) {
@@ -71,6 +133,29 @@ func Root() *cobra.Command {
 
 			if err := link.Up(rx); err != nil {
 				return errors.Wrap(err, "set link up")
+			}
+
+			var hasAddr bool
+			{
+				addrs, err := link.Addrs(rx)
+				if err != nil {
+					return errors.Wrap(err, "get link addresses")
+				}
+				for range addrs {
+					hasAddr = true
+				}
+			}
+			if !hasAddr {
+				addr := rtnl.NewAddress()
+				addr.Info.Address = &net.IPNet{
+					IP:   net.IPv4(10, 5, 5, 1),
+					Mask: net.CIDRMask(24, 32),
+				}
+				if err := link.AddAddr(rx, addr); err != nil {
+					return errors.Wrap(err, "add address to link")
+				}
+			} else {
+				lg.Info("wg-manager link already has address")
 			}
 
 			var config Config
@@ -107,14 +192,43 @@ func Root() *cobra.Command {
 				}
 				if config.Port != 0 {
 					wgConfig.ListenPort = &config.Port
+					wgConfig.FirewallMark = &config.Port
 				}
 			}
 			if err := client.ConfigureDevice(link.Info.Name, wgConfig); err != nil {
 				return errors.Wrap(err, "configure device")
 			}
 
+			if err := app.configureRouting(ctx, link.Info.Name, config.NATInterface); err != nil {
+				return errors.Wrap(err, "configure routing")
+			} else {
+				lg.Info("routing configured")
+			}
+
 			<-ctx.Done()
 			lg.Info("wg-manager stopped")
+			{
+				ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+				defer cancel()
+
+				if err := link.Down(rx); err != nil {
+					lg.Warn("set link down", "err", err)
+				}
+
+				if err := link.Del(rx); err != nil {
+					if errors.Is(err, os.ErrNotExist) {
+						lg.Info("wg-manager link already deleted")
+					} else {
+						lg.Warn("delete wireguard link", "err", err)
+					}
+				}
+
+				if err := app.deconfigureRouting(ctx, link.Info.Name, config.NATInterface); err != nil {
+					lg.Warn("deconfigure routing", "err", err)
+				} else {
+					lg.Info("routing deconfigured")
+				}
+			}
 			return ctx.Err()
 		},
 	}
